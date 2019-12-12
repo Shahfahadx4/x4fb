01f57332b3b9d2ac0c4266b3296b545c8f60bf43
Shah Fahad x4 <38689297+Shahfahadx4@users.noreply.github.com>
2019-12-13 01:19:32 +0500

    Update x4fb.py

--- x4fb.py
+++ x4fb.py
@@ -101,6 +101,7 @@ def login():
 		pwd = raw_input('\033[1;96m[+] \x1b[1;93mPassword \x1b[1;91m: \x1b[1;92m')
 		tik()
 		try:
+		
 			br.open('https://m.facebook.com')
 		except mechanize.URLError:
 			print"\n\033[1;96m[!] \x1b[1;91mNo Internet Connection"
